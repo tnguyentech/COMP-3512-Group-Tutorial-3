6911abd6e5f16c3a22943117052af0683543de32
ChrisEberleSchool <156865868+ChrisEberleSchool@users.noreply.github.com>
2026-02-27 11:16:44 -0700

    chore: added db handling

--- index.php
+++ index.php
@@ -1,9 +1,39 @@
+<!DOCTYPE html>
+
 <?php 
-// Group Members: Tony Nguyen, 
+// Group Members: Tony Nguyen, Chris Eberle
 
 
 
+$db = new PDO("sqlite:./travel.db");
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$result = $db->query("SELECT * FROM Countries");
+
+$options = [];
+
+
+echo '<select> <option value="all">All Countries</option>';
+
+while ($row = $result->fetch()) {
+    echo "<option value={$row['CountryCode']}>" . $row['CountryName'] . "</option>";
+}
+
+echo "</select>";
 
 
 
 ?>
+
+<html>
+
+<body>
+    <form method="GET">
+        <select>
+            <? php
+            ?>
+        </select>
+    </form>
+</body>
+
+</html>
