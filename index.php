152577701b05aca2da05ff7d0dc2a738d6727d4a
ChrisEberleSchool <156865868+ChrisEberleSchool@users.noreply.github.com>
2026-02-27 12:16:18 -0700

    chore: added form logic and image display

--- index.php
+++ index.php
@@ -3,37 +3,51 @@
 <?php 
 // Group Members: Tony Nguyen, Chris Eberle
 
-
-
+//db setup
 $db = new PDO("sqlite:./travel.db");
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$result = $db->query("SELECT * FROM Countries");
-
-$options = [];
-
-
-echo '<select> <option value="all">All Countries</option>';
-
-while ($row = $result->fetch()) {
-    echo "<option value={$row['CountryCode']}>" . $row['CountryName'] . "</option>";
+// selected country
+$selectedCountry = "all";
+// Update the selectedCountry
+if (isset($_GET['country'])) {
+    $selectedCountry = $_GET['country'];
 }
-
-echo "</select>";
-
-
-
 ?>
 
 <html>
 
 <body>
     <form method="GET">
-        <select>
-            <? php
+        <label>Select Country:</label>
+        <select name="country">
+            <option value="all">All Countries</option>
+            <?php
+            $result = $db->query("SELECT * FROM Countries");
+
+            while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                echo "<option value='" . $row['CountryCode'] . "'>" . $row['CountryName'] . "</option>";
+            }
             ?>
         </select>
+
+        <input type="submit">
     </form>
+
+    <?php
+    if($selectedCountry == "all") {
+        // Display all images
+        $result = $db->query("SELECT * FROM ImageDetails");
+
+            while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                echo "<img src='./images/" . $row['FileName'] . "'>" . "</img>";
+            }
+    } else {
+        // Here we will handle displaying the images associated with code
+
+    }
+    
+    ?>
 </body>
 
 </html>
